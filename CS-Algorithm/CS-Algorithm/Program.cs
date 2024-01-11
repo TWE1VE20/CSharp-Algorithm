b3b0f40a00784550ad0e9680206ab3e916b4e738
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-11 09:34:34 +0900

    기본 프로젝트 추가

---
+++ CS-Algorithm/CS-Algorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Algorithm");
+        }
+    }
+}
