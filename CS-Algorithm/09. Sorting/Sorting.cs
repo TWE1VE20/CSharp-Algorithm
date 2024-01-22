d74100eb3782f342fa86cd46b4ea4af924cc5ffc
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-22 16:01:39 +0900

    feat : Sorting 추가

---
+++ CS-Algorithm/09. Sorting/Sorting.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._Sorting
+{
+    internal class Sorting
+    {
+        // <선택정렬>
+        // 데이터 중 가장 작은 값부터 하나씩 선택하여 정렬
+        // 시간복잡도 -  O(n²)
+        // 공간복잡도 -  O(1)
+        // 안정정렬   -  O
+        public static void SelectionSort(IList<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[minIndex])
+                        minIndex = j;
+                }
+                Swap(list, i, minIndex);
+            }
+        }
+
+
+
+        // <삽입정렬>
+        // 데이터를 하나씩 꺼내어 정렬된 자료 중 적합한 위치에 삽입하여 정렬
+        // 단 n의 갯수가 16보다 작으면 퀵정렬보다 더 빠를 수 있다.
+        // 시간복잡도 -  O(n²)
+        // 공간복잡도 -  O(1)
+        // 안정정렬   -  O
+        public static void InsertionSort(IList<int> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    if (list[j - 1] < list[j])
+                        break;
+
+                    Swap(list, j - 1, j);
+                }
+            }
+        }
+
+
+
+        // <버블정렬>
+        // 서로 인접한 데이터를 비교하여 정렬
+        // 시간복잡도 -  O(n²)
+        // 공간복잡도 -  O(1)
+        // 안정정렬   -  O
+        public static void BubbleSort(IList<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 1; j < list.Count; j++)
+                {
+                    if (list[j - 1] > list[j])
+                        Swap(list, j - 1, j);
+                }
+            }
+        }
+
+
+
+        // <병합정렬>
+        // 데이터를 2분할하여 정렬 후 합병
+        // 데이터 갯수만큼의 추가적인 메모리가 필요
+        // 하지만 순서대로 읽어내기에 캐쉬 적중율이 좋아 실제로는 빠름
+        // 시간복잡도 -  O(nlogn)
+        // 공간복잡도 -  O(n)
+        // 안정정렬   -  O
+        public static void MergeSort(IList<int> list, int left, int right)
+        {
+            if (left == right) return;
+
+            int mid = (left + right) / 2;
+            MergeSort(list, left, mid);
+            MergeSort(list, mid + 1, right);
+            Merge(list, left, mid, right);
+        }
+
+        private static void Merge(IList<int> list, int left, int mid, int right)
+        {
+            List<int> sortedList = new List<int>();
+            int leftIndex = left;
+            int rightIndex = mid + 1;
+
+            // 분할 정렬된 List를 병합
+            while (leftIndex <= mid && rightIndex <= right)
+            {
+                if (list[leftIndex] < list[rightIndex])
+                    sortedList.Add(list[leftIndex++]);
+                else
+                    sortedList.Add(list[rightIndex++]);
+            }
+
+            if (leftIndex > mid)    // 왼쪽 List가 먼저 소진 됐을 경우
+            {
+                for (int i = rightIndex; i <= right; i++)
+                    sortedList.Add(list[i]);
+            }
+            else  // 오른쪽 List가 먼저 소진 됐을 경우
+            {
+                for (int i = leftIndex; i <= mid; i++)
+                    sortedList.Add(list[i]);
+            }
+
+            // 정렬된 sortedList를 list로 재복사
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                list[left + i] = sortedList[i];
+            }
+        }
+
+
+
+        // <퀵정렬>
+        // 하나의 피벗을 기준으로 작은값과 큰값을 2분할하여 정렬
+        // 최악의 경우(피벗이 최소값 또는 최대값)인 경우 시간복잡도가 O(n²)
+        // 하지만 순서대로 읽어내기에 캐쉬 적중율이 좋아 실제로는 빠름
+        // 시간복잡도 -  평균 : O(nlogn)   최악 : O(n²)
+        // 공간복잡도 -  O(1)
+        // 안정정렬   -  X
+        public static void QuickSort(IList<int> list, int start, int end)
+        {
+            if (start >= end) return;
+
+            int pivot = start;
+            int left = pivot + 1;
+            int right = end;
+
+            while (left <= right) // 엇갈릴때까지 반복
+            {
+                // pivot보다 큰 값을 만날때까지
+                while (list[left] <= list[pivot] && left < end)
+                    left++;
+                while (list[right] >= list[pivot] && right > start)
+                    right--;
+
+                if (left < right)     // 엇갈리지 않았다면
+                    Swap(list, left, right);
+                else                  // 엇갈렸다면
+                    Swap(list, pivot, right);
+            }
+
+            QuickSort(list, start, right - 1);
+            QuickSort(list, right + 1, end);
+        }
+
+
+
+        // <힙정렬>
+        // 힙을 이용하여 우선순위가 가장 높은 요소가 가장 마지막 요소와 교체된 후 제거되는 방법을 이용
+        // 배열에서 연속적인 데이터를 사용하지 않기 때문에 캐시 메모리를 효율적으로 사용할 수 없어 상대적으로 느림
+        // 캐쉬 적중률, 래지스터 비 친화적 방식이라 이론적으로 빠를수 있어도 실제로는 느리다.
+        // 이유는 하나의 큰 배열을 띄엄띄엄 보며 다니기에 캐쉬내용물을 계속 다시 로딩을 해야하기에 느려진다.
+        // 시간복잡도 -  O(nlogn)
+        // 공간복잡도 -  O(1)
+        // 안정정렬   -  X
+        public static void HeapSort(IList<int> list)
+        {
+            MakeHeap(list);
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                Swap(list, 0, i);
+                Heapify(list, 0, i);
+            }
+        }
+
+        private static void MakeHeap(IList<int> list)
+        {
+            for (int i = list.Count / 2 - 1; i >= 0; i--)
+            {
+                Heapify(list, i, list.Count);
+            }
+        }
+
+        private static void Heapify(IList<int> list, int index, int size)
+        {
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
+            int max = index;
+            if (left < size && list[left] > list[max])
+                max = left;
+            if (right < size && list[right] > list[max])
+                max = right;
+
+            if (max != index)
+            {
+                Swap(list, index, max);
+                Heapify(list, max, size);
+            }
+        }
+
+
+        private static void Swap(IList<int> list, int left, int right)
+        {
+            int temp = list[left];
+            list[left] = list[right];
+            list[right] = temp;
+        }
+
+        // 인트로 정렬 : n이 16보다 작을려우 삽입정렬 그보다 높다면 heap정렬을 사용하는 정렬로 자료구조의 특징을 이용한 정렬도 있다.
+    }
+}
